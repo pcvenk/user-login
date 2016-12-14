da051c9c74435c9d68be7828ed9eda38c74be56c
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-12-14 14:11:56 +0100

    DB connection

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressHandlebars = require('express-handlebars');
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var flash = require('connect-flash');
+var passport = require('passport');
+var mongoose = require('mongoose');
+
+var configDB = require('./config/db');
+mongoose.connect(configDB.url);
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +22,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', expressHandlebars());
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
