3ce38cf42755ec60b8216e4a22eb600e5bc8c386
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-12-14 14:18:01 +0100

    Done: middleware setup

--- app.js
+++ app.js
@@ -33,6 +33,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'passport',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
+
 app.use('/', routes);
 app.use('/users', users);
 
