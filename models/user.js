cec25a982a620d8526063ff0e2d1d8b190ed66d8
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-12-15 16:49:55 +0100

    Done: user schema and defining functions for getting a user, comparing passwords and adding a user

---
+++ models/user.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+var userSchema = new mongoose.Schema({
+
+    username: String,
+    email: String,
+    password: String,
+    name: String,
+    joinDate: Date,
+    updatedAt: {
+        type: Date,
+        default: Date.now()
+    }
+
+});
+
+var User = module.exports = mongoose.model('User', userSchema);
+
+// Get User by ID
+
+module.exports.getUserById = function(id, callback){
+
+    User.findById(id, callback);
+
+};
+
+// Compare password
+
+module.exports.comparePassword = function(comparePassword, hash, callback){
+
+    bcrypt.compare(comparePassword, hash, function(err, isMatch){
+       if(err){
+           return callback(err);
+       } else{
+           callback(null, isMatch);
+       }
+    });
+
+};
+
+// Add User
+
+module.exports.addUser = function(user, callback){
+
+    User.create(user, callback);
+
+};
