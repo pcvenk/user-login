2e18a381884c874770414989b22ceff3d6e3325a
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2016-12-16 16:42:34 +0100

    Done: passport registration done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
